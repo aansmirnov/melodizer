e149e9d2192aea04b22c30de14bc653680de54cc
aansmirnov <aansmrnv@gmail.com>
2024-11-09 02:02:07 +0400

    flexible error message

--- lib.ts
+++ lib.ts
@@ -1,7 +1,7 @@
 export function getElementByID(id: string) {
   const element = document.getElementById(id);
 
-  if (!element) throw new Error(`Element with ${id} was not found!`);
+  if (!element) throw new Error(`Element with id="${id}" was not found!`);
 
   return element;
 }
