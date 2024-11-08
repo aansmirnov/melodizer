b406dfef00d32aae5f4ee2fa99529d187f1939f8
aansmirnov <aansmrnv@gmail.com>
2024-11-09 00:28:53 +0400

    get the play frequency of a song

--- index.ts
+++ index.ts
@@ -1,23 +1,47 @@
 import { getElementByID } from "./lib";
 
 const uploaderElement = getElementByID("uploader");
-const audioElement = getElementByID("audio") as HTMLAudioElement;
 
-function loadFile(event: Event) {
+const audioContext = new AudioContext();
+let source = audioContext.createBufferSource();
+
+let analyser = audioContext.createAnalyser();
+analyser.fftSize = 256;
+
+let dataArray: Uint8Array;
+
+async function loadFile(event: Event) {
   const [file] = (event.target as HTMLInputElement).files || [];
 
   if (!file) throw new Error("File not found!");
 
-  addFileToSource(file);
+  await playSong(file);
 }
 
-function addFileToSource(file: File) {
-  audioElement.src = URL.createObjectURL(file);
+async function playSong(file: File) {
+  const buffer = await file.arrayBuffer();
+  const audioData = await audioContext.decodeAudioData(buffer);
+
+  source.buffer = audioData;
+
+  const bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+  analyser.connect(audioContext.destination);
+  source.start();
+
+  draw();
 }
 
-function playMusic() {
-  audioElement.play();
+function draw() {
+  // @ToDO
 }
+
+// setInterval(() => {
+  //   console.log(analyser.getByteFrequencyData(dataArray));
+  //   console.log(dataArray);
+  //   console.log(source.context.currentTime, source.buffer?.duration);
+// }, 500);
 
 uploaderElement.addEventListener("change", loadFile);
-audioElement.addEventListener("canplaythrough", playMusic);
